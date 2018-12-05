a4fed67255f7e6ca6b178e06ef1971c79cabc9cd
Ritwik Sharma <ritwik.1204@gmail.com>
2018-12-05 23:32:21 +0530

    Add training model

---
+++ hub/examples/image_retraining/label_image.py
@@ -0,0 +1,37 @@
+import tensorflow as tf, sys
+from subprocess import Popen
+import os
+
+image_path = sys.argv[1]
+
+# Read in the image_data
+image_data = tf.gfile.FastGFile(image_path, 'rb').read()
+
+# Loads label file, strips off carriage return
+label_lines = [line.rstrip() for line in tf.gfile.GFile("./retrained_labels.txt")]
+
+# Unpersists graph from file
+with tf.gfile.FastGFile("./retrained_graph.pb", 'rb') as f:
+    graph_def = tf.GraphDef()
+    graph_def.ParseFromString(f.read())
+    _ = tf.import_graph_def(graph_def, name='')
+    
+# Feed the image_data as input to the graph and get first prediction
+with tf.Session() as sess:
+    softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+    predictions = sess.run(softmax_tensor, {'DecodeJpeg/contents:0': image_data})
+    # Sort to show labels of first prediction in order of confidence
+    top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
+    for node_id in top_k:
+        human_string = label_lines[node_id]
+        score = predictions[0][node_id]
+        print "\n--------------------------------------------------------"
+        print('%s (score = %.5f)' % (human_string, score))
+        print '  %.5f' % (score*100),"%"
+        print "--------------------------------------------------------"
+image_preview="display "+image_path
+os.system(image_preview)        
+print "\n"
+ans = raw_input("Want to know more about this image? y/n \n")
+if ans.rstrip() is "y":
+    Popen(["python", "reverse-image-search.py", image_path])
