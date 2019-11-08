f9fefea0801c16870090329904db2e45442669f7
Andrew Attard <andrew.attard@outlook.com>
2019-11-08 11:38:37 +0700

    Updated number format to 3 digits (000, 001, 002 etc.), ensured that each directory counted separately

--- utils/rename_files.py
+++ utils/rename_files.py
@@ -12,7 +12,7 @@ for folderName, subfolders, filenames in os.walk('.\\training_data'):
 		absWorkingDir = os.path.abspath('C:\\dev_projects\\contributions\\Celestial-bodies-detection\\hub\\examples\\image_retraining')
 		print(absWorkingDir)
 		src = os.path.join(absWorkingDir, folderName, filename)
-		dst = os.path.join(absWorkingDir, folderName, str(i)) + '.jpg'
+		dst = os.path.join(absWorkingDir, folderName, str("{0:0=3d}".format(i))) + '.jpg'
 		print('Renaming "%s" to "%s"...' % (src, dst))
 		shutil.move(src, dst)
 		i += 1
