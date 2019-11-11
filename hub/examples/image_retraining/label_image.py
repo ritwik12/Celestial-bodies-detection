673674b489ffe91b7c869e13d22567c8be34c426
DarenHammer <darenhammer@localhost-live.attlocal.net>
2019-11-11 17:09:09 -0600

    added vesta asteroid

--- hub/examples/image_retraining/label_image.py
+++ hub/examples/image_retraining/label_image.py
@@ -118,6 +118,13 @@ def wiki(celestial_object):
         statistics = "\n".join(['-- {}: {}'.format(parameter, value) for parameter, value in req_statistics.items()])
         print("{}\n\n".format(statistics))
         print(wikipedia.WikipediaPage(title='{}'.format("433_"+(ans.capitalize()))).summary)
+    elif ans == 'vesta':
+        print("--------------------------------------------------------")
+        print("Classified Celestial Object is the {} Asteroid : ".format(ans.capitalize()))
+        print("-------------------------------------------------------- \n")
+        statistics = "\n".join(['-- {}: {}'.format(parameter, value) for parameter, value in req_statistics.items()])
+        print("{}\n\n".format(statistics))
+        print(wikipedia.WikipediaPage(title='{}'.format("4_"+(ans.capitalize()))).summary)
     return " "
 
 print(wiki(celestial_object))
