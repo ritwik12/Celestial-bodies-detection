6120cdf6f2ee4c9782b56799abae9355ead78fda
Satyabrat Bhol <sbhol@fastenal.com>
2020-02-05 18:15:33 +0530

    fixes

--- tests/test_build.py
+++ tests/test_build.py
@@ -5,6 +5,6 @@ from hub.examples.image_retraining.retrain import *
 def test_class_count_value(image_dir):
     testing_percentage = 10
     validation_percentage = 10
-    list_of_images = create_image_lists(None.image_dir, testing_percentage, validation_percentage)
+    list_of_images = create_image_lists(image_dir, testing_percentage, validation_percentage)
     count_value = len(list_of_images.keys())
     assert count_value > 1
