36c6f9475d87b60ae5e30d8b8ff1760ceaea9379
Satyabrat Bhol <satyabrat35@gmail.com>
2020-02-06 12:04:34 +0530

    Travis Build (#55)
    * travis build
    
    * travis build
    
    * pytest
    
    * fix .travis
    
    * fix .travis
    
    * tests
    
    * Tests added
    
    * Fix .travis
    
    * Fix .travis
    
    * fixes

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+
+def pytest_addoption(parser):
+    """ Adds parser for pytest testing """
+    parser.addoption("--image_dir", action="store", default="./training_data")
+
+@pytest.fixture(scope='session')
+def image_dir(request):
+    dir_val = request.config.option.image_dir
+    if dir_val is None:
+        pytest.fail()
+    return dir_val
